3485300592dffff999f2d0813a1ffab86fe9c7f1
cdelauder <cdelauder@gmail.com>
2014-09-13 16:55:56 -0700

    we were not able to access the funtions of the module before because we werent instantiating an instance of the module. now we hav figured out what the correct structure is

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var server = express()
+var todoModule = require('./js/model.js')
+var todo = new todoModule()
+
+
 
 server.get('/', function(req, res) {
+  todo.all()
   res.set('Content-Type', 'text/html');
   res.sendFile(__dirname + '/index.html')
 })
--- js/model.js
+++ js/model.js
@@ -1,11 +1,19 @@
 module.exports = todo
 
 function todo() {
-  var db = require('')
+  this.db = require('mongodb').MongoClient;
+
+  // Connect to the db
+  this.db.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+    if(!err) {
+    console.log("We are connected");
+  }
+});
 }
 
 todo.prototype = {
   all: function() {
+  console.log('fuck yeah')
     //get all the todos
   },
 
