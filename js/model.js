b3723e288b2cf4dcaa6d274ecfa57ba04816e851
cdelauder <cdelauder@gmail.com>
2014-09-13 16:05:02 -0700

    added the model module

---
+++ js/model.js
@@ -0,0 +1,15 @@
+module.exports = todo
+
+function todo() {
+  var db = require('')
+}
+
+todo.prototype = {
+  all: function() {
+    //get all the todos
+  },
+
+  create: function() {
+    // create todo and return single todo
+  }
+}
