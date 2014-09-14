3b9d82f833d765386446e816e0431b3f369c3b2a
cdelauder <cdelauder@gmail.com>
2014-09-14 13:28:58 -0700

    was able to finally successfully write to the database. had to make a connection to the colelction first

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var express = require('express')
 var server = express()
 var todoModule = require('./js/model.js')
 var todo = new todoModule()
+var querystring = require('querystring')
 
 
 
 server.get('/', function(req, res) {
-  todo.all()
   res.set('Content-Type', 'text/html');
   res.sendFile(__dirname + '/index.html')
 })
+
+server.post('/new', function(req, res) {
+  req.on('data', function (chunk) {
+    var body = chunk.toString()
+    var form = querystring.parse(body)
+    todo.create(newTodo(form))
+  })
+})
+
+function newTodo(formdata) {
+  var data = {title: formdata['title'],
+              content: formdata['content']}
+  return data
+}
 
 
 server.listen(3000)
--- js/model.js
+++ js/model.js
@@ -4,20 +4,33 @@ function todo() {
   this.db = require('mongodb').MongoClient;
 
   // Connect to the db
-  this.db.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(!err) {
-    console.log("We are connected");
-  }
+  this.collection = this.db.connect("mongodb://localhost:27017/db", function(err, db) {
+    if(err) {
+      console.log("We fucked up yo!");
+    }
+    return db.collection('junk')
 });
+  console.log(this.collection)
+
 }
 
 todo.prototype = {
   all: function() {
-  console.log('fuck yeah')
+  return this.collection.find()
     //get all the todos
   },
 
-  create: function() {
-    // create todo and return single todo
+  create: function(doc) {
+    this.db.connect("mongodb://localhost:27017/db", function(err, db) {
+    if(err) {
+      console.log("We fucked up yo!");
+    }
+    db.collection('stuff').insert(doc, function(err) {
+      console.log(err)
+    })
+    // return db.collection('junk')
+});
+    console.log(this.collection)
+    // console.log(this.collection.find())
   }
 }
